7302ee7214582856c495a0c07b571800cdef4b03
staronthepark <asdffg6934@naver.com>
2022-05-07 23:41:34 +0900

    MAP

--- Assets/Scripts/MapFactory.cs
+++ Assets/Scripts/MapFactory.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 
 public class MapFactory : MonoBehaviour
 {
-    [SerializeField]
-    private float m_TileSizeX;
-    [SerializeField]
-    private float m_TileSizeY;
-    [SerializeField]
-    private float m_TileSizeZ;
-    [SerializeField]
-    private float m_wallHeight;
+    [SerializeField] private float m_TileSizeX;
+    [SerializeField] private float m_TileSizeZ;
+    [SerializeField] private float m_wallHeight;
 
-    [SerializeField]
-    private GameObject[] m_TileOBJ;
-    [SerializeField]
-    private GameObject[] m_CeilingOBJ;
-    [SerializeField]
-    private GameObject m_WallOBJ;
+    [SerializeField] private GameObject[] m_TileOBJ;
+    [SerializeField] private GameObject[] m_CeilinOBJ;
+    [SerializeField] private GameObject m_WallOBJ;
+    [SerializeField] private GameObject m_ParentsOBJ;
 
-    private const int m_MaxLine = 100;
-    private int m_TileCount;
-
-    private bool[,] TileisEmpty = new bool[m_MaxLine, m_MaxLine];
+    private GameObject[] m_ExitTileOBJ = new GameObject[2];
 
     private Stack<Tile> m_tileStack = new Stack<Tile>();
     private Queue<Tile> m_tileQueue = new Queue<Tile>();
+
+    private int m_TileCount;
+
+    private const int m_MaxTileCount = 100;
+    private const int m_MaxLine = 20;
+
+    private bool[,] m_TileisEmpty = new bool[m_MaxLine, m_MaxLine];
+
+    private float m_distance = 0;
+
+    private Tile m_lastPoint;
 
     class Tile
     {
@@ -39,18 +40,33 @@ public class MapFactory : MonoBehaviour
         }
     }
 
-    void CreateTile(int x, int z)
+    void CreateTile(int x, int z, int objindex)
     {
         int type = Random.Range(0, 4);
         Tile newTile = new Tile(x, z);
 
-        Instantiate(m_TileOBJ[type], new Vector3(x * m_TileSizeX, 0, z * m_TileSizeZ), Quaternion.Euler(0, 0, 0));
-        Instantiate(m_TileOBJ[type], new Vector3(x * m_TileSizeX, m_TileSizeY * 2, z * m_TileSizeZ), Quaternion.Euler(0, 0, 0));
+        Vector3 pos = new Vector3(x * m_TileSizeX, 0, z * m_TileSizeZ);
+
+        GameObject[] tile = new GameObject[2];
+        tile[0] = Instantiate(m_TileOBJ[objindex], pos, Quaternion.Euler(0, 0, 0));
+        tile[1] = Instantiate(m_TileOBJ[objindex], new Vector3(x * m_TileSizeX, m_wallHeight, z * m_TileSizeZ), Quaternion.Euler(0, 0, 0));
+
+        tile[0].transform.parent = m_ParentsOBJ.transform;
+        tile[1].transform.parent = m_ParentsOBJ.transform;
+
+        float dist = Vector3.Distance(Vector3.zero, pos);
+
+        if (m_distance < dist)
+        {
+            m_distance = dist;
+            m_lastPoint = new Tile(x, z);
+            m_ExitTileOBJ = tile;
+        }
 
         m_tileStack.Push(newTile);
         m_tileQueue.Enqueue(newTile);
 
-        TileisEmpty[x, z] = false;
+        m_TileisEmpty[x, z] = false;
         m_TileCount++;
     }
 
@@ -85,7 +101,9 @@ public class MapFactory : MonoBehaviour
         if (dir - 2 < 0)
             angle = 90;
 
-        Instantiate(m_WallOBJ, new Vector3(x, m_TileSizeY, z), Quaternion.Euler(0, angle, 0));
+        GameObject wallOBJ = Instantiate(m_WallOBJ, new Vector3(x, m_wallHeight / 2, z), Quaternion.Euler(0, angle, 0));
+        wallOBJ.transform.localScale = new Vector3(m_TileSizeX, m_wallHeight, m_TileSizeZ);
+        wallOBJ.transform.parent = m_ParentsOBJ.transform;
     }
 
     int FindEmpty(int _x, int _z, int createType)
@@ -106,12 +124,12 @@ public class MapFactory : MonoBehaviour
 
             if(createType == 0)
             {
-                for (int i = 0; i < 4; i++)
+                int i = 0;
+                while (i < 4)
                 {
                     dir = Random.Range(0, 4);
-                    if (dir == usedDir[i]) continue;
-                    usedDir[count] = dir;
-                    count++;
+                    if (dir == usedDir[i++]) continue;
+                    usedDir[count++] = dir;
                     break;
                 }
             }
@@ -143,9 +161,9 @@ public class MapFactory : MonoBehaviour
                 if (z < 0) z = 0;
                 if (z >= m_MaxLine) z = m_MaxLine - 1;
 
-                if (TileisEmpty[x, z])
+                if (m_TileisEmpty[x, z])
                 {
-                    CreateTile(x, z);
+                    CreateTile(x, z, 0);
                     return count;
                 }
             }
@@ -153,7 +171,7 @@ public class MapFactory : MonoBehaviour
             {
                 if(x > -1 && x < m_MaxLine && z > -1 && z < m_MaxLine)
                 {
-                    if(TileisEmpty[x, z])
+                    if(m_TileisEmpty[x, z])
                     CreateWall(_x, _z, dir);
                 }
                 else
@@ -171,12 +189,8 @@ public class MapFactory : MonoBehaviour
         int x = 0;
         int z = 0;
 
-        int random = 0;
-
-        while (m_TileCount < 100)
+        while (m_TileCount < m_MaxTileCount)
         {
-            random = Random.Range(0, 4);
-
             if(FindEmpty(x, z, 0) < 4)
             {
                 x = m_tileStack.Peek().x;
@@ -226,12 +240,15 @@ public class MapFactory : MonoBehaviour
         {
             for (int j = 0; j < m_MaxLine; j++)
             {
-                TileisEmpty[i, j] = true;
+                m_TileisEmpty[i, j] = true;
             }
         }
                 
-        CreateTile(0, 0);
+        CreateTile(0, 0, 2);
         TileCreateLoop();
         WallCreateLoop();
+        Destroy(m_ExitTileOBJ[0]);
+        Destroy(m_ExitTileOBJ[1]);
+        CreateTile(m_lastPoint.x, m_lastPoint.z, 2);
     }
 }
